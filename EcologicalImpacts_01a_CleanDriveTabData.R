9f390f9737104efaff8c6945d04477eaada4b480
madelineaberg <57677697+madelineaberg@users.noreply.github.com>
2024-04-29 12:19:23 -0700

    Add files via upload

---
+++ EcologicalImpacts_01a_CleanDriveTabData.R
@@ -0,0 +1,213 @@
+# Bringing in tabular data only & cleaning it to combine with non-projected points -------------
+# November 1, 2021
+
+# Packages needed ---------------------------------------------------------
+library(sf)
+library(tidyverse)
+library(raster)
+library(here)
+library(ggplot2)
+library(viridis)
+
+#############################################################################################
+################################# OBS TAB ###################################################
+#############################################################################################
+
+# Bring in tabular data ---------------------------------------------------------
+tab_data <- read.csv(here::here("datatemp/original/driving.obs.csv"))
+
+# remove test obs
+tab_data <- filter(tab_data, route != "test") # remove test obs
+
+# remove obs from the week with missing points (2019-06-15) 
+# has no GPS points, so don't want to include in weeks but depending on what doing might not want to remove this
+tab_data <- filter(tab_data, date != "2019-06-15") 
+
+# Check dataset & variables ------------------------------------------------
+table(tab_data$NG.use.code)
+
+# Put the date variable into the date format
+tab_data$date <- as.Date(tab_data$date, "%d/%m/%Y") # want R to read this as a date
+
+# Put time into time format 
+tab_data$time1
+tab_data <- mutate(tab_data, time1 = case_when(
+  time <= 999 ~ paste0("0", time), 
+  time >= 1000 ~ paste0(time))) 
+tab_data$TimeDiff <- difftime(as.POSIXct(tab_data$time1, format = "%H%M"), 
+                              as.POSIXct("0:00", format = "%H:%M"), units = "min")
+
+# Route length - exact, calculated in QGIS
+tab_data <- tab_data %>%
+  mutate(rt_length = case_when(
+    route == "BB1" ~ 19.99, 
+    route == "BB2" ~ 13.56, 
+    route == "CC" ~ 16.00,
+    route == "GV" ~ 17.34,
+    route == "PV1" ~ 12.91,
+    route == "PV2" ~ 13.60,
+    route == "PV3" ~ 14.20,
+    route == "SC" ~ 16.21,
+    route == "SF1" ~ 11.12,
+    route == "SF2" ~ 18.69))
+
+# add obs variable 
+tab_data$obs <- "MA"
+
+# trying to add a sequential week variable for each week through the study
+tab_data <- tab_data %>% group_by(date) %>% dplyr::mutate(week = cur_group_id())
+table(tab_data$week, tab_data$date) ## seems like this worked 10/17/21
+table(tab_data$week)
+
+# adding year and month variables
+tab_data <- tab_data %>%
+  mutate(
+    year = case_when(
+      grepl("2021", date) ~ "2021",
+      grepl("2020", date) ~ "2020",
+      grepl("2019", date) ~ "2019"), 
+    month = case_when(
+      grepl("-01-", date) ~ "January",
+      grepl("-02-", date) ~ "February",
+      grepl("-03-", date) ~ "March",
+      grepl("-04-", date) ~ "April",
+      grepl("-05-", date) ~ "May",
+      grepl("-06-", date) ~ "June",
+      grepl("-07-", date) ~ "July",
+      grepl("-08-", date) ~ "August",
+      grepl("-09-", date) ~ "September",
+      grepl("-10-", date) ~ "October",
+      grepl("-11-", date) ~ "November",
+      grepl("-12-", date) ~ "December"),
+    year.num = case_when(
+      year == "2019" ~ 1,
+      year == "2020" ~ 2, 
+      year == "2021" ~ 3),
+    month.num = case_when(
+      grepl("-01-", date) ~ 1,
+      grepl("-02-", date) ~ 2,
+      grepl("-03-", date) ~ 3,
+      grepl("-04-", date) ~ 4,
+      grepl("-05-", date) ~ 5,
+      grepl("-06-", date) ~ 6,
+      grepl("-07-", date) ~ 7,
+      grepl("-08-", date) ~ 8,
+      grepl("-09-", date) ~ 9,
+      grepl("-10-", date) ~ 10,
+      grepl("-11-", date) ~ 11,
+      grepl("-12-", date) ~ 12), 
+    Day.of.Week = "S")
+
+table(tab_data$survey.id)
+
+table(tab_data$use.type) # the codes I used - more description than NG use codes
+table(tab_data$NG.use.code)
+
+table(tab_data$NG.habitat.over)
+tab_data$NG.habitat.over <- recode(tab_data$NG.habitat.over, "road" = "none")
+
+table(tab_data$NG.habitat.under)
+tab_data$NG.habitat.under <- recode(tab_data$NG.habitat.under, "none" = "bare")
+tab_data$NG.habitat.under <- recode(tab_data$NG.habitat.under, "Perrenial grass" = "perrenial grass")
+tab_data$NG.habitat.under <- recode(tab_data$NG.habitat.under, " " = "bare")
+tab_data$NG.habitat.under <- recode(tab_data$NG.habitat.under, "nA" = "unknown")
+table(tab_data$NG.habitat.under)
+
+table(tab_data$num.people)
+table(tab_data$num.men)
+tab_data$num.men <- as.integer(tab_data$num.men)
+table(tab_data$num.women)
+table(tab_data$num.kids)
+tab_data$num.kids <- as.integer(tab_data$num.kids)
+
+tab_data$distance <- recode(tab_data$distance, "500+"="500")
+tab_data$distance <- as.integer(tab_data$distance)
+
+str(tab_data$bearing) # looks good
+
+# fix some route id names
+tab_data$survey.id <- recode(tab_data$survey.id, 
+                             "PV1-28918" = "PV1-28919",
+                             "SF1-090521" = "SF1-090520", 
+                             "SF1-090522" = "SF2-090520",
+                             "SF2-090521" = "SF2-090520",
+                             "SF2-090522" = "SF2-090520")
+
+head(tab_data)
+str(tab_data)
+
+#############################################################################################
+################################# Route TAB #################################################
+#############################################################################################
+
+# Bring in the route data -------------------------------------------------
+route_tab_data <- read.csv(here::here("datatemp/original/driving.routes.csv"))
+
+# Filtering out test routes
+route_tab <- filter(route_tab_data, route != "CC-TEST" & route !="SF-TEST")
+
+# Fix the mislabeled route 
+route_tab$route <- recode(route_tab$route, "SF1 " = "SF1")
+route_tab$route <- recode(route_tab$route, "PV1 " = "PV1")
+route_tab$route <- recode(route_tab$route, "PV 3" = "PV3")
+route_tab$route <- as.factor(route_tab$route)
+
+# fix mislabeled survey id
+route_tab$survey.id <- recode(route_tab$survey.id, 
+                              "B1-040420" = "BB1-040420", 
+                              "GV-030720" = "GV-070320", 
+                              "PV1-030621" = "PV1-060321")
+
+# check variables & make necessary changes
+route_tab$numeric_num_obs <- as.numeric(route_tab$num.obs)
+route_tab$numeric_num_shoot <- as.numeric(route_tab$num.shoot)
+
+str(route_tab$date)
+route_tab$Date <- as.Date(route_tab$date, "%d %B %Y")
+str(route_tab$Date)
+
+
+route_tab$start.time <- recode(route_tab$start.time, 
+                               "1413 - 1432" = "1413",
+                               "1440-1501" = "1440", 
+                               "1420-1445" = "1420", 
+                               "1144-1200" = "1144")
+route_tab$start.time2 <- as.numeric(route_tab$start.time)
+route_tab <- mutate(route_tab, time1 = case_when(
+  start.time2 <= 999 ~ paste0("0", start.time2), 
+  start.time2 >= 1000 ~ paste0(start.time2))) 
+route_tab$TimeDiff <- difftime(as.POSIXct(route_tab$time1, format = "%H%M"), 
+                              as.POSIXct("0:00", format = "%H:%M"), units = "min")
+
+# Route length - exact, calculated in QGIS
+route_tab <- route_tab %>%
+  mutate(rt_length = case_when(
+    route == "BB1" ~ 19.99, 
+    route == "BB2" ~ 13.56, 
+    route == "CC" ~ 16.00,
+    route == "GV" ~ 17.34,
+    route == "PV1" ~ 12.91,
+    route == "PV2" ~ 13.60,
+    route == "PV3" ~ 14.20,
+    route == "SC" ~ 16.21,
+    route == "SF1" ~ 11.12,
+    route == "SF2" ~ 18.69))
+
+route_tab <- route_tab %>%
+  mutate(
+    year = case_when(
+      grepl("2021", date) ~ "2021",
+      grepl("2020", date) ~ "2020",
+      grepl("2019", date) ~ "2019")) 
+
+library(weathermetrics)
+route_tab$tempC <- fahrenheit.to.celsius(route_tab$temp)
+
+table(route_tab$NG.weather)
+table(route_tab$month)
+
+
+
+
+
+
---
+++ EcologicalImpacts_01b_RecreationIntensityFunctions.R
@@ -0,0 +1,210 @@
+#### Recreation Intensity Functions ####
+     #### November 19, 2021 ####
+
+#### UPDATE: Feb 2024
+  # the package 'maptools' has been archived
+install.packages("maptools", repos = "https://packagemanager.posit.co/cran/2023-10-13")
+library(maptools)
+
+
+
+#### Final Workflow Goals ####
+# 1 - Load Packages
+# 2 - Load Spatial & Tabular Data * tab data should go through CleanTabData.R first * 
+# 3 - Make 2 versions of each necessary spatial data - Lat/Long & UTM 
+# 4 - Join driving (lat/long) & cleaned tabular data & filter by observation type (all, rec, shoot, hunt, target)
+# 5 - Subset driving data by time periods **** 
+# 6 - Adjust point locations (PointAdjust function + need to make iterative)
+# 7 - Kernel density map - raster- extract - save extraction values in a dataframe ****
+
+#### adjust points function ####
+# This function is using the distance and bearing we recorded during driving routes to fix the pt locations
+# Before using, make sure that distance and bearing are numeric variables
+# Just need to input a dataframe that is an sf object in Lat/Long CRS w/ bearing & distance & geometry info
+
+PointAdjust <- function(df){ # df needs to be sf object, CRS = LatLong, WGS84
+  library(sf)
+  library(sp)
+  library(geosphere)
+  sp <- as_Spatial(df) # Make into a spatial object, so we can use the package
+  adjust <- SpatialPoints(destPoint(sp, df$bearing, df$distance)) # Adjust points w/ distance & bearing
+  proj4string(adjust) <- proj4string(sp) # Add CRS back to points
+  return(adjust) # returns the adjusted points 
+}
+
+#test_PA <- PointAdjust(driving_all_np) # seems to be working!! 
+#test_PA
+
+#### Kernel density - Extraction Function ####
+
+KD_extract <- function(W, P, d, E, b){
+  # W = window or boundary for the extraction (sf object, NAD83/UTM zone 11N)
+  # P = driving observation points (Spatial Points, Lat/Long WGS84) * should be adjusted points from PointAdjust()
+  # d = distance in meters for the sigma of the kernel density (have been using 1000)
+  # E = points to use in the raster extraction (paired sites, nests) - sf object, NAD83 UTM zone 11
+  # b = buffer size for raster extraction in meters
+  library(sf)
+  library(spatstat)
+  library(raster)
+  library(maptools)
+  library(rgdal)
+  W.sp <- as_Spatial(W) # boundary - make a spatial object
+  WW <- as(W.sp, "owin") # boundary - set as "owin" to match package format
+  P.proj <- spTransform(P, proj4string(W.sp)) # project to CRS of the boundary
+  P.proj.ppp <- as(P.proj, "ppp") # make "ppp" to match package format
+  marks(P.proj.ppp) <- NULL # set all marks to null
+  Window(P.proj.ppp) <- WW # define the study area
+  KD <- density(P.proj.ppp, sigma = d) # kernel density
+  plot(KD)
+  sp_dat <- as.data.frame(KD) # store kernel density as a dataframe
+  kd_raster <- rasterFromXYZ(sp_dat, crs = crs(W.sp)) # make kernel density dataframe into a raster
+  # extract kernel density raster values with in b meters of E (paired site, nest)
+  raster_extract <- raster::extract(kd_raster, as(E, "Spatial"), buffer = b, fun = mean)  
+  return(raster_extract)
+}
+
+
+#### Kernel density - extraction function + trying to save raster ####
+
+KD_extractsave <- function(W, P, d, E, b){
+  # W = window or boundary for the extraction (sf object, NAD83/UTM zone 11N)
+  # P = driving observation points (Spatial Points, Lat/Long WGS84) * should be adjusted points from PointAdjust()
+  # d = distance in meters for the sigma of the kernel density (have been using 1000)
+  # E = points to use in the raster extraction (paired sites, nests) - sf object, NAD83 UTM zone 11
+  # b = buffer size for raster extraction in meters
+  library(sf)
+  library(spatstat)
+  library(raster)
+  library(maptools)
+  W.sp <- as_Spatial(W) # boundary - make a spatial object
+  WW <- as(W.sp, "owin") # boundary - set as "owin" to match package format
+  P.proj <- spTransform(P, proj4string(W.sp)) # project to CRS of the boundary
+  P.proj.ppp <- as(P.proj, "ppp") # make "ppp" to match package format
+  marks(P.proj.ppp) <- NULL # set all marks to null
+  Window(P.proj.ppp) <- WW # define the study area
+  KD <- density(P.proj.ppp, sigma = d) # kernel density
+  plot(KD)
+  sp_dat <- as.data.frame(KD) # store kernel density as a dataframe
+  kd_raster <- rasterFromXYZ(sp_dat, crs = crs(W.sp)) # make kernel density dataframe into a raster
+  
+  # save the raster in the DrivingRasters folder
+  f <- paste0('raster', 99)
+  writeRaster(kd_raster, filename = file.path("C:/Users/Madeline/Desktop/EcologicalImpactsManuscript", f), format = "GTiff", overwrite = TRUE)
+  
+  # extract kernel density raster values with in b meters of E (paired site, nest)
+  raster_extract <- raster::extract(kd_raster, as(E, "Spatial"), buffer = b, fun = mean)  
+  return(raster_extract)
+}
+
+
+
+
+#############################33
+#### Kernel density - extraction function + trying to save raster - splitting up ####
+## don't like thissssss
+# currently the same as KD_extractsave
+KD_funct <- function(W, P, d, E, b){
+  # W = window or boundary for the extraction (sf object, NAD83/UTM zone 11N)
+  # P = driving observation points (Spatial Points, Lat/Long WGS84) * should be adjusted points from PointAdjust()
+  # d = distance in meters for the sigma of the kernel density (have been using 1000)
+  # E = points to use in the raster extraction (paired sites, nests) - sf object, NAD83 UTM zone 11
+  # b = buffer size for raster extraction in meters
+  library(sf)
+  library(spatstat)
+  library(raster)
+  library(maptools)
+  W.sp <- as_Spatial(W) # boundary - make a spatial object
+  WW <- as(W.sp, "owin") # boundary - set as "owin" to match package format
+  P.proj <- spTransform(P, proj4string(W.sp)) # project to CRS of the boundary
+  P.proj.ppp <- as(P.proj, "ppp") # make "ppp" to match package format
+  marks(P.proj.ppp) <- NULL # set all marks to null
+  Window(P.proj.ppp) <- WW # define the study area
+  KD <- density(P.proj.ppp, sigma = d) # kernel density
+  plot(KD)
+  sp_dat <- as.data.frame(KD) # store kernel density as a dataframe
+  kd_raster <- rasterFromXYZ(sp_dat, crs = crs(W.sp)) # make kernel density dataframe into a raster
+  
+  # save the raster in the DrivingRasters folder
+  f <- paste0('raster', 99, '.tif')
+  writeRaster(kd_raster, filename = file.path("C:/Users/Madeline/Desktop/EcologicalImpactsManuscript", f))
+  
+  # extract kernel density raster values with in b meters of E (paired site, nest)
+  raster_extract <- raster::extract(kd_raster, as(E, "Spatial"), buffer = b, fun = mean)  
+  return(raster_extract)
+}
+
+#KD_extractsave(NCA_utm, shoot.adj, 1000, PairedSites_utm, 2000)
+
+#e <- paste0('month', 2, '.tif')
+#file.rename(paste0("/Users/madelineaberg/Desktop/Driving Analysis/DrivingRasters/", "raster99.tif"),
+         #   paste0("/Users/madelineaberg/Desktop/Driving Analysis/DrivingRasters/", e))
+
+
+
+extract_save <- function(KD) { 
+  plot(KD)
+  sp_dat <- as.data.frame(KD) # store kernel density as a dataframe
+  kd_raster <- rasterFromXYZ(sp_dat, crs = crs(W.sp)) # make kernel density dataframe into a raster
+  
+  # save the raster in the DrivingRasters folder
+  f <- paste0('raster', i, '.tif')
+  writeRaster(kd_raster, filename = file.path("C:/Users/Madeline/Desktop/EcologicalImpactsManuscript", f))
+  
+  # extract kernel density raster values with in b meters of E (paired site, nest)
+  raster_extract <- raster::extract(kd_raster, as(E, "Spatial"), buffer = b, fun = mean)  
+  return(raster_extract)
+}
+
+#KD_funct(NCA_utm, shoot.adj, 1000, PairedSites_utm, 2000)
+#file.rename(paste0(my_path, file_names_old),       # Rename files
+ #           paste0(my_path, file_names_new))
+
+############################# code graveyard below ##################$
+#N = 155
+#KD_extractsave(NCA_utm, shoot.adj, 1000, PairedSites_utm, 2000, N)
+
+
+# seems to be the same....???? .grd vs .tif
+#test <- raster("/Users/madelineaberg/Desktop/Driving Analysis/raster12.grd")
+#test.tif <- raster("/Users/madelineaberg/Desktop/Driving Analysis/raster12.tif")
+
+
+## testing out ggplot with the test.tif & .grd test - both plot, need to fix scale but generally ok
+#library(ggplot2)
+# library(rasterVis)
+
+#gplot(test) + 
+ # geom_tile(aes(fill=value)) +
+  #ggtitle("r1") +
+  #scale_fill_gradient2(low="red", mid="green", high="blue") 
+                     #  midpoint=mean(value),    # Value that gets the middle color (default is zero)
+                      # breaks=seq(-100,100,10))  # Set whatever breaks you want
+                      # limits=c(floor(rng[1]), ceiling(rng[2])))  # Set the same limits for each plot
+
+
+
+
+
+
+
+#test_KD <- KD_extract(NCA_utm, shoot.all, 1000, PairedSites_utm, 2000) #### WORKING 10/17/21 !!!!!!! Works w/ results from PointAdjust()
+#test_KD2 <- KD_extract(NCA, alldrive.sp.adj, 1000, PairedSites_p, 2000)
+
+#### should add a command to save the KD raster / plot somewhere #### 
+
+
+
+#### Trying to subset by time periods ####
+# just random scraps of stuff below here 
+
+#driving_all_np # format of the datasets at this point
+
+#str(driving_all_np$date)
+#min(driving_all_np$date) #2019-04-05
+#max(driving_all_np$date) #2021-07-17
+#table(driving_all_np$date)
+
+#max(driving_all_np$week)
+
+
+# testing with year (10/20/2021)
